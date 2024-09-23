d99e186a0c86e07ef75ba40ef443a8bc71cce48e
jmpark1115 <jmpark1115@gmail.com>
2024-09-23 14:27:43 +0900

    init

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ window.onload = () => {
     const headerEl = document.querySelector('header')
     const mobMenuBtn = document.querySelector('header .hamburgerIcon')
     mobMenuBtn.addEventListener('click', () => headerEl.classList.toggle('clicked'))
-    if(localStorage.getItem('Email') !== '247and.a@gmail.com') {
+//    if(localStorage.getItem('Email') !== '247and.a@gmail.com') {
+    if(localStorage.getItem('Email') !== 'jmpark1115@gmail.com') {
         alert('Access denied. You are not Authorized to access this page.')
         location.replace('/')
     }
